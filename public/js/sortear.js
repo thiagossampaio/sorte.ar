66b7a5fa5a512de19c3ae67a6bbda5e1c3f3b8b9
Thiago Sampaio <thiagonego@gmail.com>
2014-07-17 01:13:11 -0300

    colocando o numero de participacoes do usuario

--- public/js/sortear.js
+++ public/js/sortear.js
@@ -1,6 +1,7 @@
 var fotoPrincipal = jQuery('#fotoPrincipal').html();
 var comentarios = jQuery('#comentarios').html();
 var itens, quantidadeItens, rouletter, option, sorteado;
+var c;
 
 jQuery(document).ready(function(){
     
@@ -18,9 +19,9 @@ jQuery(document).ready(function(){
 	    method : 'GET',
 	    dataType : 'jsonp',
 	    success : function(data){
-		Mustache.parse(fotoPrincipal);
-		var renderedFotoPrincipal = Mustache.render(fotoPrincipal, data.data);
-		jQuery('#fotos-container').append(renderedFotoPrincipal);
+			Mustache.parse(fotoPrincipal);
+			var renderedFotoPrincipal = Mustache.render(fotoPrincipal, data.data);
+			jQuery('#fotos-container').append(renderedFotoPrincipal);
 	    }
 	
 	});
@@ -40,6 +41,8 @@ jQuery(document).ready(function(){
 		    Mustache.parse(comentarios);
 		    
 		    if (data) {
+
+				c = data.data;
 
 				jQuery(data.data).each(function(i){
 				    
@@ -78,13 +81,28 @@ function gerarRoullet(){
 		    jQuery('#sorteio_vencedor_nome').text('');
 		    jQuery('#sorteio_vencedor_usuario').text('');
 		    jQuery('#sorteio_data').text('');
+			jQuery('#sorteio_participacoes').text('');
 		},
 		slowDownCallback : function(){
 		},
 		stopCallback : function($stopElm){
+			console.log($stopElm);
 		    jQuery('.roulette_container').addClass('well-sorteado');
 		    jQuery('#sorteio_vencedor_nome').text($stopElm.data('full-name'));
 		    jQuery('#sorteio_vencedor_usuario').text('@'+ $stopElm.data('username'));
+		    jQuery('#sorteio_data').text(new Date().format('dd/MM/yyyy'));
+
+		    var username = $stopElm.data('username');
+		    console.log(username);
+		    var count = 0;
+			jQuery.each(c, function(i, v) {
+			    if (v.from.username == username) {
+			        count++;
+			        console.log(count);
+			    }
+			});
+			jQuery('#sorteio_participacoes').text(count);
+
 		}
     }
 
@@ -92,11 +110,11 @@ function gerarRoullet(){
     rouletter.roulette(option);
     
     jQuery('.start').click(function(){
-	rouletter.roulette('start');
+		rouletter.roulette('start');
     });
     
     jQuery('.stop').click(function(){
-	rouletter.roulette('stop');
+		rouletter.roulette('stop');
     });
     
 }
